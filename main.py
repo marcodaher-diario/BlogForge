0c932caa09949ce2ea58a28e09269c449c7db68d
marcodaher-diario <marco.daher@gmail.com>
2026-02-13 19:55:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import json
+import random
 from core.scheduler import blogs_do_dia
 
 def carregar_config_blog(nome_blog):
     caminho = f"blogs/{nome_blog}/config.json"
     with open(caminho, "r", encoding="utf-8") as f:
         return json.load(f)
+
+def carregar_temas(nome_blog):
+    caminho = f"blogs/{nome_blog}/temas.txt"
+    with open(caminho, "r", encoding="utf-8") as f:
+        temas = [linha.strip() for linha in f if linha.strip()]
+    return temas
 
 def main():
     blogs = blogs_do_dia()
@@ -23,9 +30,19 @@ def main():
         print(f"Blog ID: {blog_id}")
 
         config = carregar_config_blog(nome)
-
         print("Config carregado:")
         print(config)
+
+        temas = carregar_temas(nome)
+
+        if not temas:
+            print("Nenhum tema encontrado!")
+            print("-" * 40)
+            continue
+
+        tema_escolhido = random.choice(temas)
+
+        print(f"Tema escolhido: {tema_escolhido}")
         print("-" * 40)
 
 if __name__ == "__main__":
