4ded223f78d1894244c486016b8be896ecff102e
marcodaher-diario <marco.daher@gmail.com>
2026-02-14 01:41:46 -0300

    Update scheduler.py

--- core/scheduler.py
+++ core/scheduler.py
@@ -1,24 +1,7 @@
-import json
-from datetime import datetime
-
-def carregar_config():
-    with open("blogs_config.json", "r", encoding="utf-8") as f:
-        return json.load(f)
-
 def blogs_do_dia():
-    config = carregar_config()
-
-    hoje = datetime.now()
-    dia_semana = hoje.isoweekday()  # 1=Segunda ... 7=Domingo
-
-    blogs_para_publicar = []
-
-    for nome_blog, dados in config.items():
-        if dia_semana in dados["dias"]:
-            blogs_para_publicar.append({
-                "nome": nome_blog,
-                "blog_id": dados["blog_id"]
-            })
-
-    return blogs_para_publicar
-
+    return [
+        {"nome": "emagrecer", "blog_id": "5251820458826857223"},
+        {"nome": "dfbolhas", "blog_id": "4324376157396303471"},
+        {"nome": "mdartefoto", "blog_id": "5852420775961497718"},
+        {"nome": "diariodenoticias", "blog_id": "8284393764979637984"},
+    ]
